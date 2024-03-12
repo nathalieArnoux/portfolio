4447ec2add349ea4e1b25aa5f0b507259bcd4d6e
Nathalie <arnouxn@gmail.com>
2024-03-12 19:13:56 +0100

    init contact branch

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -3,6 +3,7 @@ import Hero from "../Hero/Hero";
 import About from "../About/About";
 import Skills from "../Skills/Skills";
 import Portfolio from "../Portfolio/Portfolio";
+import Contact from "../Contact/Contact";
 import Footer from "../Footer/Footer";
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <About />
         <Skills />
         <Portfolio />
+        <Contact />
       </main>
       <Footer />
     </div>
---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,9 @@
+const Contact = () => {
+  return (
+    <section id="contact">
+      <p>contact</p>
+    </section>
+  );
+};
+
+export default Contact;
