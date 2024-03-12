a53b9947f96771692e060e703b4fcaa349cc6f0b
Nathalie <arnouxn@gmail.com>
2024-03-12 18:59:16 +0100

    init about branch

---
+++ src/components/About/About.jsx
@@ -0,0 +1,9 @@
+const About = () => {
+  return (
+    <section id="about">
+      <p>about</p>
+    </section>
+  );
+};
+
+export default About;
--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,5 +1,6 @@
 import Header from "../Header/Header";
 import Hero from "../Hero/Hero";
+import About from "../About/About";
 import Footer from "../Footer/Footer";
 
 const App = () => {
@@ -8,6 +9,7 @@ const App = () => {
       <Header />
       <main>
         <Hero />
+        <About />
       </main>
       <Footer />
     </div>
